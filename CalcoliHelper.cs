50f0cb7e9452e296a4bcffbde5bae5c02f22d183
andreadldev <andrea.delisi2@gmail.com>
2023-03-22 14:59:51 +0100

    Calcoli helper

---
+++ CalcoliHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public static class CalcHelper
+    {
+        public static int Somma(int num1, int num2) => num1 + num2;
+        public static double Somma(double num1, double num2) => num1 + num2;
+
+        public static int Differenza(int num1, int num2) => num1 - num2;
+        public static double Differenza(double num1, double num2) => num1 - num2;
+
+        public static int Moltiplicazione(int num1, int num2) => num1 * num2;
+        public static double Moltiplicazione(double num1, double num2) => num1 * num2;
+
+        public static int ValoreAssoluto(int num) => Math.Abs(num);
+        public static double ValoreAssoluto(double num) => Math.Abs(num);
+
+        public static int Minimo(int num1, int num2) => num1 < num2 ? num1 : num2;
+        public static double Minimo(double num1, double num2) => num1 < num2 ? num1 : num2;
+
+        public static int Massimo(int num1, int num2) => num1 > num2 ? num1 : num2;
+        public static double Massimo(double num1, double num2) => num1 > num2 ? num1 : num2;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//SOMMA
+Console.WriteLine($"Somma numeri interi: {CalcHelper.Somma(3, 2)}");
+Console.WriteLine($"Somma numeri double: {CalcHelper.Somma(3.0, 2.5)}");
+
+//DIFFERENZA
+Console.WriteLine($"Differenza numeri interi: {CalcHelper.Differenza(3, 2)}");
+Console.WriteLine($"Differenza numeri double: {CalcHelper.Differenza(3.0, 2.5)}");
+
+//MOLTIPLICAZIONE
+Console.WriteLine($"Moltiplicazione numeri interi: {CalcHelper.Moltiplicazione(3, 2)}");
+Console.WriteLine($"Moltiplicazione numeri double: {CalcHelper.Moltiplicazione(3.0, 2.5)}");
+
+//VALORE ASSOLUTO
+Console.WriteLine($"Valore assoluto numero intero: {CalcHelper.ValoreAssoluto(-5)}");
+Console.WriteLine($"Valore assoluto numero intero: {CalcHelper.ValoreAssoluto(-3.5)}");
+
+//MINIMO
+Console.WriteLine($"Minimo tra due numeri interi: {CalcHelper.Minimo(4, 8)}");
+Console.WriteLine($"Minimo tra due numeri double: {CalcHelper.Minimo(4.5, 8.5)}");
+
+//MASSIMO
+Console.WriteLine($"Minimo tra due numeri interi: {CalcHelper.Massimo(4, 8)}");
+Console.WriteLine($"Minimo tra due numeri double: {CalcHelper.Massimo(4.5, 8.5)}");
